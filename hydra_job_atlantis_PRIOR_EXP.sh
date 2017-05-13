85c074cb51550e8f0c8bacbc6439fbbbad3b9fd2
Yannick Merckx <dockwekerym@gmail.com>
2017-05-13 15:28:02 +0200

    added batch files cluster

---
+++ hydra_job_atlantis_PRIOR_EXP.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+# number of nodes and cores 
+#PBS -l nodes=1:ppn=1
+# memory requirements change this when using more/less experience replay samples
+#PBS -l mem=16gb
+# max run time
+#PBS -l walltime=200:00:00
+# output and error files
+#PBS -o atl_PRIOR_EXP.out
+#PBS -e atl_PRIOR_EXP.err
+#PBS -N atl_PRIOR_EXP
+#PBS -V
+
+module add openblas
+cd $HOME
+source .bashrc
+source activate dqn
+cd DQN-tensorflow
+python main.py --env_name=Atlantis-v0 --is_train=True --double_q=False --use_gpu=False  --priority_exp=True
